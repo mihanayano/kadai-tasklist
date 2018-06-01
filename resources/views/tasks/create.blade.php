894690c37d45a74f20c7981d03e09a1aa4240fa7
EC2 Default User <ec2-user@ip-172-31-5-153.us-east-2.compute.internal>
2018-06-01 06:34:49 +0000

    saiteishutsu

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,18 +4,20 @@
 
 <!-- Write content for each page here -->
 <h1>New Tasklist Page</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-
-        {!! Form::label('content', 'task:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('Submit') !!}
-
-    {!! Form::close() !!}
+ <div class="row">
+    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+        {!! Form::model($task, ['route'=>'tasks.store']) !!}
+            <div class="form-group">
+                {!! Form::label('content', 'task') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('status', 'status') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
+    </div>
+</div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,20 +2,25 @@
 
 @section('content')
 
+<div class="container">
 <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
-
+<div class="row">
+    
+    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-        
-        {!! Form::label('status', 'ステータス') !!}
-        {!! Form::text('status') !!}
-
-        
-        {!! Form::label('content', 'tasklist:') !!}
-        {!! Form::text('content') !!}
-
+        <div class="form-group">
+        {!! Form::label('status', 'status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+         </div>
+        <div class="form-group">
+        {!! Form::label('content', 'tasklist') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
         {!! Form::submit('refresh') !!}
 
     {!! Form::close() !!}
-
+        </div>
+</div>
+</div>
 @endsection
